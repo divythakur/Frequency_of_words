94eb7e157c9e0ceaab7a71a7cadff83858d735e7
divythakur <divyanshuthakur75@gmail.com>
2020-08-07 19:07:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import axios from "axios";
 import './App.css';
+import Heading from './Heading'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Terrible extends Component{
+    constructor(){
+    super()
+    this.state={
+        num:0,
+        wordslist:[]
+    
+    };
+    }
+     onClickHandler=(e)=>{
+         var data="";
+            axios
+            .get("https://terriblybackend.herokuapp.com/data")
+            .then((res) => {
+               data =res.data;
+               var myArray =data.split('↵').join(', ').split(':').join(', ').split(' ').join(', ').split('/').join(', ').split('@').join(', ').split('-').join(', ').split('.').join(', ').split('?').join(', ').split(')').join(', ').split(','); 
+               var frequency={};var i;
+               for(i=0;i<myArray.length;i++)
+               {
+                   if( myArray[i] in frequency && myArray[i]!=" " && myArray[i]!=""  )
+                   {
+                       frequency[myArray[i]]++;
+                   }
+                   else{
+                       if(myArray[i]!=" " && myArray[i]!="")
+                       {
+                    frequency[myArray[i]]=1;
+                       }
+                   }
+
+               }
+               var n=Object.keys(frequency).length
+               //console.log(n)
+                var sortedfrequency= Object.entries(frequency).sort(function(a,b){return b[1]-a[1]})
+               console.log(this.state.num)
+
+                var t=`<table><tr><th>Word</th><th>Frequency</th></tr>`;
+        for(i=0;i<this.state.num;i++)
+        {
+            t+=`<tr>`+
+                `<td>`+sortedfrequency[i][0]+`</td>`+
+                `<td>`+sortedfrequency[i][1]+`</td>`+
+                 `</tr>`;
+        }
+        t+='</table>';
+        document.getElementById("table-wrapper").innerHTML=t;
+                
+                 
+
+            })
+           
+
+       e.preventDefault()   
+       }
+    
+          onchangeHandler=(e)=>{
+            this.setState({ num: e.target.value })
+
+          }
+    
+
+    render(){
+        return (
+            <div>
+                <Heading />
+                <form id="form-div">
+                    <label>Enter any number Here :-    </label>
+                    
+                    <input type="text" onChange={this.onchangeHandler} required/><br />
+                    <button type="submit" onClick={this.onClickHandler}>Search</button>
+                </form>
+                <div id="table-wrapper" style={{marginLeft:600,marginTop:100}}></div>
+               
+            </div>
+
+        );
+    }
+
+    
 }
-
-export default App;
+export default Terrible;
---
+++ src/Heading.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+function Heading(){
+    return(
+    <h1 style={{textAlign:"center"}}>Frequency Dictionary</h1>
+    );
+}
+export default Heading;
